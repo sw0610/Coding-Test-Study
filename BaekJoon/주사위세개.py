c6df37810f4e9576e1e506c36c7fbc526df43ab8
sw0610 <enthdnjs11@gmail.com>
2022-04-02 20:53:54 +0900

    0402

---
+++ BaekJoon/주사위세개.py
@@ -0,0 +1,10 @@
+a, b, c=map(int, input().split())
+
+if a==b==c:
+    print(10000+a*1000)
+elif a==b or a==c:
+    print(1000+a*100)
+elif b==c:
+    print(1000+b*100)
+elif a!=b and b!=c and a!=c:
+    print(max(a,b,c)*100)
