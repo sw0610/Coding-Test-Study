4d5376939a1b8f82b92e87cc87a6d6bd650954c2
sw0610 <enthdnjs11@gmail.com>
2022-04-29 23:56:20 +0900

    0429

---
+++ BaekJoon/N번째큰수.py
@@ -0,0 +1,20 @@
+import sys
+import heapq
+n=int(input())
+heap=[]
+
+for i in range(n):
+    nums=list(map(int,sys.stdin.readline().split()))
+
+    if not heap:
+        for num in nums:
+            heapq.heappush(heap, num)
+    else:
+        for num in nums:
+            if heap[0]<num:
+                heapq.heappush(heap, num)
+                heapq.heappop(heap)
+        
+
+print(heap[0])
+    
