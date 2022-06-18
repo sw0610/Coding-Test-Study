da8c2efffdab9a2fcf962ffa4eabef37a42e2958
sw0610 <enthdnjs11@gmail.com>
2022-06-19 01:25:33 +0900

    0619

--- BaekJoon/prac.py
+++ BaekJoon/prac.py
@@ -1,36 +1,6 @@
 n=int(input())
+d=[0,1,2]
 
-board=[]
-for i in range(n):
-    board.append(list(map(int, input())))
-
-dx=[0,0,-1,1]
-dy=[-1,1,0,0]
-
-
-def dfs(x, y):
-    if x<0 or y<0 or x>=n or y>=n:
-        return False
-    if board[x][y]==1:
-        global cnt
-        board[x][y]=0
-        cnt+=1
-        for i in range(4):
-            nx=x+dx[i]
-            ny=y+dy[i]
-            dfs(nx, ny)
-        return True
-    return False
-res=0
-cnt=0
-ans=[]
-for i in range(n):
-    for j in range(n):
-        if dfs(i, j)==True:
-            ans.append(cnt)
-            res+=1
-            cnt=0
-ans.sort()
-print(res)
-for i in range(len(ans)):
-    print(ans[i])
+for i in range(3, n+1):
+    d.append(d[i-1]+d[i-2])
+print(d[n]%10007)
